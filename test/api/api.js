2c79e04de39b82899ebba11890c5d0afebd3427a
renevanderark <r.van.der.ark@gmail.com>
2015-09-21 13:49:37 +0200

    async test for onTileLoad

--- test/api/api.js
+++ test/api/api.js
@@ -19,6 +19,7 @@ describe("Api", () => {
 	});
 
 	it("should determine which image tiles to fetch based on a given top/left position, viewport dimensions scale and level and call fetchTile with makeTiles");
+	it("should handle an image error properly with onTileLoad");
 
 	it("should construct properly");
 	it("should recursively initialize resolution levels from config with initializeResolutions");
@@ -26,8 +27,20 @@ describe("Api", () => {
 	it("should generate a valid djatoka image URL based on a valid tile spec with makeTileUrl");
 	it("should divide a given value val by 2 a given amount of times with downScale");
 	it("should multiply a given value val by 2 a given amount of times with upScale");
-	it("should wait for the property complete on the image object and call onTile on complete with onTileLoad");
-	it("should handle an image error properly with onTileLoad");
+	it("should wait for the property complete on the image object to be true and call onTile on complete with onTileLoad", (done) => {
+		let tileIm = {complete: false, src: "mock:data"};
+		let tileSpec = "mock:data";
+		let onTile = function(im, tileObj) {
+			expect(im).toEqual(tileIm);
+			expect(tileObj).toEqual(tileSpec);
+			done();
+		};
+		setTimeout(() => { tileIm.complete = true; }, 100);
+		api.onTileLoad(tileIm, tileSpec, onTile);
+
+	});
+
+
 	it("should cache an image tile in the tileMap and call onTileLoad with fetchTile", () => {
 		let tile = {realX: 1, realY: 2, pos: {x: 3, y: 4}, level: 5, url: ":url:"};
 		let onTileCalled = false;
@@ -83,7 +96,7 @@ describe("Api", () => {
 	});
 
 
-	it("should determine scale, resolution level and dimensions for the full image based on a desired zoom at full resolution with zoomTo", () => {
+	it("should determine scale, resolution level and dimensions for the full image based on a desired zoom at full resolution with zoomTo", (done) => {
 		let onScaleCalled = false;
 		let zoom = 0.4;
 		let expectedScale = zoom * 2;
@@ -98,7 +111,6 @@ describe("Api", () => {
 		api.zoomTo(zoom, onScale);
 		expect(onScaleCalled).toBe(true);
 
-		onScaleCalled = false;
 		zoom = 0.2;
 		expectedScale = zoom * 2 * 2;
 		expectedLevel = apiConfig.levels - 2;
@@ -108,29 +120,27 @@ describe("Api", () => {
 			expect(l).toEqual(expectedLevel);
 			expect(w).toEqual(apiConfig.width * zoom);
 			expect(h).toEqual(apiConfig.height * zoom);
+			done();
 		};
 		api.zoomTo(zoom, onScale);
-		expect(onScaleCalled).toBe(true);
 
 	});
 
 
-	it("should determine scale, resolution level and dimensions for the full image based on the current full scale + a given factor with zoomBy", () => {
-		let onScaleCalled = false;
+	it("should determine scale, resolution level and dimensions for the full image based on the current full scale + a given factor with zoomBy", (done) => {
 		let l = 4;
 		let s = 0.4;
 		let f = 1.1;
 		let sToVpS = s / 2 + f;
 		let expectedLevel = 5;
 		let onScale = function(s1, l1, w, h) {
-			onScaleCalled = true;
 			expect(s1).toEqual(sToVpS);
 			expect(l1).toEqual(expectedLevel);
 			expect(w).toEqual(apiConfig.width * sToVpS);
 			expect(h).toEqual(apiConfig.height * sToVpS);
+			done();
 		};
 		api.zoomBy(f, s, l, onScale);
-		expect(onScaleCalled).toBe(true);
 	});
 
 
