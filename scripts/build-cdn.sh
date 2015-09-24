3d9509e952d1ed32cc94d6a7472c9836e5e8916c
renevanderark <r.van.der.ark@gmail.com>
2015-09-24 15:46:37 +0200

    new standalone release 1.6.0

--- scripts/build-cdn.sh
+++ scripts/build-cdn.sh
@@ -5,7 +5,7 @@
 echo "Exporting standalone for version: $npm_package_version"
 
 node_modules/.bin/browserify \
-	--require react > build/react-libs.js
+	--require react | node_modules/.bin/uglifyjs > build/react-libs.js
 
 node_modules/.bin/browserify src/standalone.jsx \
 	--extension=.jsx \
@@ -13,7 +13,7 @@ node_modules/.bin/browserify src/standalone.jsx \
 	 --transform [ babelify ] \
   	-t brfs \
   	--verbose \
-	--external react > build/pack.js
+	--external react | node_modules/.bin/uglifyjs > build/pack.js
 
 
 cat build/react-libs.js build/pack.js > build/hire-djatoka-client-$npm_package_version.js
