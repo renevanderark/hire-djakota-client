cea740db337f56d6e951163e36a1e5b1fa24e458
renevanderark <r.van.der.ark@gmail.com>
2015-09-30 14:58:55 +0200

    tests for touch events on zoom component and rerendering on zoom component

--- test/integration/zoom.js
+++ test/integration/zoom.js
@@ -48,6 +48,27 @@ describe("Zoom", () => {
 		zoomComponent.onMouseDown({pageX: 178, pageY: 10});
 	});
 
+	it("should trigger zoom on touch start", function(done) {
+		let calls = 0;
+		let unsubscribe = store.subscribe(() => {
+			function finalize(e) {
+				zoomComponent.onMouseUp();
+				unsubscribe();
+				done(e);
+			}
+
+			calls++;
+			let state = store.getState().realViewPort;
+			try {
+				expect(state.zoom).to.be.above(1.0);
+				expect(state.zoom).to.be.below(1.3);
+			} catch(e) { finalize(e); }
+			if(calls === 2) { finalize(); }
+		});
+
+		zoomComponent.onTouchStart({touches: [{pageX: 200, pageY: 10}], preventDefault: function() {}})
+	});
+
 	it("should trigger zoom on mouse move", function(done) {
 		let calls = 0;
 		let unsubscribe = store.subscribe(() => {
@@ -71,6 +92,32 @@ describe("Zoom", () => {
 		});
 		zoomComponent.onMouseDown({pageX: 10, pageY: 10});
 		zoomComponent.onMouseMove({pageX: 178, pageY: 10, preventDefault: function() {}});
+	});
+
+
+	it("should trigger zoom on touch move", function(done) {
+		let calls = 0;
+		let unsubscribe = store.subscribe(() => {
+			function finalize(e) {
+				zoomComponent.onMouseUp();
+				unsubscribe();
+				done(e);
+			}
+			calls++;
+			let state = store.getState().realViewPort;
+			if(calls === 2) {
+				try { expect(state.zoom).to.be.below(0.3); } catch(e) { finalize(e); }
+			}
+			if(calls === 4) {
+				try {
+					expect(state.zoom).to.be.above(0.99);
+					expect(state.zoom).to.be.below(1.01);
+				} catch(e) { finalize(e); }
+				finalize();
+			}
+		});
+		zoomComponent.onTouchStart({touches: [{pageX: 10, pageY: 10}], preventDefault: function() {}})
+		zoomComponent.onTouchMove({touches: [{pageX: 178, pageY: 10}], preventDefault: function() {}});
 	});
 
 	it("should trigger zoom on mouse wheel", function(done) {
@@ -104,4 +151,33 @@ describe("Zoom", () => {
 		zoomComponent.onWheel({deltaY: 1, preventDefault: function() {}});
 	});
 
+
+	it("should update zoom level info when viewport has zoomed", function(done) {
+		let initalZoom = store.getState().realViewPort.zoom;
+		let initialZoomLabelText = React.findDOMNode(zoomComponent).querySelector("label span").innerHTML;
+		let initialCirclePosition = React.findDOMNode(zoomComponent).querySelector("circle").getAttribute("cx");
+		let calls = 0;
+		let unsubscribe = store.subscribe(() => {
+			function finalize(e) {
+				unsubscribe();
+				done(e);
+			}
+			if(++calls === 50) {
+				try { 
+					let newZoomLabelText = React.findDOMNode(zoomComponent).querySelector("label span").innerHTML;
+					let newCirclePosition = React.findDOMNode(zoomComponent).querySelector("circle").getAttribute("cx");
+					expect(parseInt(newCirclePosition)).to.be.above(parseInt(initialCirclePosition));
+					expect(parseInt(newZoomLabelText)).to.be.above(parseInt(initialZoomLabelText));
+					expect(store.getState().realViewPort.zoom).to.be.above(initalZoom);
+				} catch(e) { 
+					finalize(e); 
+				}
+				finalize();
+			}
+		});
+
+		for(let i = 0; i < 50; i++) {
+			viewComponent.onWheel({nativeEvent: {deltaY: -1}, preventDefault: function() {}});
+		}
+	});
 });
